635d8e77e9b86b103af8c1599893440abbdcd32c
Diego Arroyo <diegoramiroarroyo@gmail.com>
2026-04-11 16:54:09 -0300

    Nuevos files

---
+++ TP_3/Punto.java
@@ -0,0 +1,22 @@
+public class Punto {
+    int x;
+    int y;
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public static void main(String[] args) {
+        System.out.println("TP 3");
+    }
+
+}
+
